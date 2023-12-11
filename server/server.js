d231331cab0dd4e8b79b1411adbad0625a09f94c
Chris Egersdoerfer <cegersdoerfer@gmail.com>
2023-12-11 17:32:09 -0500

    t

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 const port = 4000;
 
 // set up cors to allow us to accept requests from our client
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: process.env.CLIENT_ORIGIN }));
 
 
 // Middleware to parse JSON request bodies
